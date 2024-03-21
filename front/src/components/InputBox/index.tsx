cc9a18dcaeccc96f3015ee7c592155f873f0c196
kyunggwan <4562sky@naver.com>
2024-03-21 17:50:21 +0900

    feat: 공통 컴포넌트 생성

--- front/src/components/InputBox/index.tsx
+++ front/src/components/InputBox/index.tsx
@@ -1,21 +1,60 @@
-import { forwardRef } from "react";
+import { ChangeEvent, forwardRef } from "react";
 import "./style.css";
 
-interface Props {}
+interface Props {
+  title: string;
+  placeholder: string;
+  type: "text" | "password";
+  value: string;
+  message?: string;
+  isErrorMessage: boolean;
+  buttonTitle?: string;
+  onChange: (Event: ChangeEvent<HTMLInputElement>) => void;
+  onKeydown?: (Event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onButtonClick?: () => void;
+}
 
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+  const {
+    title,
+    placeholder,
+    type,
+    value,
+    message,
+    isErrorMessage,
+    buttonTitle,
+    onChange,
+    onKeydown,
+    onButtonClick,
+  } = props;
+
+  const buttonClass =
+    value === "" ? "input-box-button-disable" : "input-box-button";
+  const messageclass = isErrorMessage
+    ? "input-box-message-error"
+    : "input-box-message";
+
   return (
     <div className="input-box">
-      <div className="input-box-title">{"아이디"}</div>
+      <div className="input-box-title">{title}</div>
       <div className="input-box-content">
         <div className="input-box-body">
           <input
+            ref={ref}
             className="input-box-input"
-            placeholder="아이디를 입력해주세요."
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeydown}
           />
-          <div className="input-box-button-disable">{"중복 확인"}</div>
+          {buttonTitle !== undefined && onButtonClick !== undefined && (
+            <div className={buttonClass} onClick={onButtonClick}>
+              {buttonTitle}
+            </div>
+          )}
         </div>
-        <div className="input-box-message-error">{"사용 가능한 아이디입니다."}</div>
+        {message !== undefined && <div className={messageclass}>{message}</div>}
       </div>
     </div>
   );
