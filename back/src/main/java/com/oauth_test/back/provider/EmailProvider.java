bc88c1af211c734ce60c69a4568d32c9b781ba6e
kyunggwan <4562sky@naver.com>
2024-03-25 23:59:30 +0900

    feat: Mail 서버

---
+++ back/src/main/java/com/oauth_test/back/provider/EmailProvider.java
@@ -0,0 +1,46 @@
+package com.oauth_test.back.provider;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class EmailProvider {
+
+    private final JavaMailSender javaMailSender;
+
+    private final String SUBJECT = "[테스트 서비스] 인증 메일입니다."; // 메일 제목
+
+    public boolean sendCertificationMail(String email, String certificationNumber) {
+
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+            String htmlContent = getCertificationMessage(certificationNumber);
+
+            messageHelper.setTo(email);
+            messageHelper.setSubject(SUBJECT);
+            messageHelper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return false;
+
+        }
+        return true;
+    }
+
+    private String getCertificationMessage(String certificationNumber){
+
+        String certificationMessage = "";
+        certificationMessage += "<h1 style='text-align: center;'> [테스트 서비스] 인증메일</h1>";
+        certificationMessage += "<h1 style='text-align: center;'> dlswmdzhem: <strong style='font-size: 32px; letter-spacing: 8px:'>" + certificationNumber + "</strong></h3>";
+        return certificationMessage;
+    }
+}
