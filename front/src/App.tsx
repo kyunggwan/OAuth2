7d93ff8ab26fe99408325346b56ff1031ee74a71
kyunggwan <4562sky@naver.com>
2024-03-29 00:12:48 +0900

    feat: 회원가입 페이지 구조 작성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,17 @@
 import React, { ChangeEvent, useState } from "react";
 import "./App.css";
-import InputBox from "components/InputBox";
+
+import { Route, Routes } from "react-router-dom";
+import SignUp from "views/Authentication/SignUp/assets";
 
 function App() {
-  const [id, setId] = useState<string>("");
-
-  const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setId(value);
-  };
-  const onIdButtonClickHandler = () => {};
-
   return (
     <>
-      <InputBox
-        title="아이디"
-        placeholder="아이디를 입력해주세요"
-        type="text"
-        value={id}
-        onChange={onIdChangeHandler}
-        buttonTitle="중복 확인"
-        onButtonClick={onIdButtonClickHandler}
-        message="사용 가능한 아이디 입니다."
-        isErrorMessage={false}
-      />
-
-      <div className="text-link-lg full-width">회원가입</div>
-      <div className="kakao-sign-in-button"></div>
-      <div className="naver-sign-in-button"></div>
-      <div className="google-sign-in-button"></div>
+      <Routes>
+        <Route path="/auth">
+          <Route path="sign-up" element={<SignUp />} />
+        </Route>
+      </Routes>
     </>
   );
 }
